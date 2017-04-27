77e06a3faa4089897e91bff9753b018bf185008c
Mosaix <mosaix@private.com>
2017-04-27 15:49:01 -0500

    Add shaders to the editor so they can be set, and hide shaders and advanced settings in a foldout to reduce clutter.

--- Assets/Mosaix/Scripts/MosaixEditor.cs
+++ Assets/Mosaix/Scripts/MosaixEditor.cs
@@ -5,6 +5,8 @@ using System.Collections;
 [CustomEditor(typeof(Mosaix))]
 public class MosaixEditor: Editor
 {
+    bool ShowShaders = false;
+    bool ShowAdvanced = false;
     public override void OnInspectorGUI()
     {
         Mosaix obj = (Mosaix) target;
@@ -26,10 +28,24 @@ public class MosaixEditor: Editor
             obj.MaskFade = EditorGUILayout.Slider("Mask Fade", obj.MaskFade, 0, 1);
         }
 
-        EditorGUILayout.LabelField("Advanced settings", EditorStyles.boldLabel);
-        obj.ShadowsCastOnMosaic = EditorGUILayout.Toggle("Shadows Cast On Mosaic", obj.ShadowsCastOnMosaic);
-        obj.HighResolutionRender = EditorGUILayout.Toggle("High Resolution Render", obj.HighResolutionRender);
-        obj.Alpha = EditorGUILayout.Slider("Alpha", obj.Alpha, 0, 1);
+        GUIStyle boldFoldout = new GUIStyle(EditorStyles.foldout);
+        boldFoldout.fontStyle = FontStyle.Bold;
+        ShowShaders = EditorGUILayout.Foldout(ShowShaders, "Shaders", true, boldFoldout);
+
+        if(ShowShaders)
+        {
+            obj.ExpandEdgesShader = (Shader) EditorGUILayout.ObjectField("Expand Edges Shader", obj.ExpandEdgesShader, typeof(Shader), false);
+            obj.MosaicShader = (Shader) EditorGUILayout.ObjectField("Mosaic Shader", obj.MosaicShader, typeof(Shader), false);
+            obj.PremultiplyShader = (Shader) EditorGUILayout.ObjectField("Premultiply Shader", obj.PremultiplyShader, typeof(Shader), false);
+        }
+
+        ShowAdvanced = EditorGUILayout.Foldout(ShowAdvanced, "Advanced settings", true, boldFoldout);
+        if(ShowAdvanced)
+        {
+            obj.ShadowsCastOnMosaic = EditorGUILayout.Toggle("Shadows Cast On Mosaic", obj.ShadowsCastOnMosaic);
+            obj.HighResolutionRender = EditorGUILayout.Toggle("High Resolution Render", obj.HighResolutionRender);
+            obj.Alpha = EditorGUILayout.Slider("Alpha", obj.Alpha, 0, 1);
+        }
     }
 }
  
