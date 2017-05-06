c5b6471bd0e17eb5847da05a253a211b113110ae
Mosaix <mosaix@private.com>
2017-05-06 03:04:54 -0500

    Add a demo project.  This makes it easier to see what this does, but Test scene is better as an example of how to set it up since it doesn't have as much extra stuff in it.

---
+++ Assets/Demo/DemoGUI.cs
@@ -0,0 +1,145 @@
+using UnityEngine;
+
+public class DemoGUI: MonoBehaviour 
+{
+    bool Anchoring = false;
+    bool AnchorScaling = false;
+    bool ShowSphere = true;
+    bool ShowOutlines = false;
+    float MosaicBlocks = 10;
+
+    public Mosaix MosaixComponent;
+    public GameObject SphereMask;
+    public Material MosaicMaterial, MosaicWithOutlineMaterial;
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(50); // left padding
+                GUILayout.BeginVertical();
+                    GUILayout.FlexibleSpace(); // bottom align
+                    GUILayout.Label("Hold ALT to navigate with the mouse", GUI.skin.box);
+                    GUILayout.Space(50); // bottom padding
+                GUILayout.EndVertical();
+            GUILayout.Space(50); // right padding
+            GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            GUILayout.Space(10); // top padding
+            GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if(GUILayout.Button("Open on GitHub"))
+                    Application.OpenURL("https://github.com/unity-effects/mosaix");
+            GUILayout.Space(10); // right padding
+            GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+
+        GUIStyle PaddedBox = new GUIStyle(GUI.skin.box); 
+        PaddedBox.padding = new RectOffset(10,10,10,10);
+
+        GUILayout.BeginArea(new Rect(10, 10, Screen.width-20, Screen.height-20));
+            GUILayout.BeginVertical(PaddedBox, GUILayout.MinWidth(250));
+            ControlWindow();
+            GUILayout.EndVertical();
+        GUILayout.EndArea();
+
+        Refresh();
+    }
+
+    void ControlWindow()
+    {
+        GUILayout.Label("Teapot Mosaic Controls");
+
+        MosaixComponent.enabled = GUILayout.Toggle(MosaixComponent.enabled, "Enable mosaic");
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Blocks");
+        MosaicBlocks = GUILayout.HorizontalSlider(MosaicBlocks, 2, 100);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Alpha");
+        MosaixComponent.Alpha = GUILayout.HorizontalSlider(MosaixComponent.Alpha, 0, 1);
+        GUILayout.EndHorizontal();
+
+        ShowOutlines = GUILayout.Toggle(ShowOutlines, "Outlines");
+
+        Anchoring = GUILayout.Toggle(Anchoring, "Anchoring");
+        if(Anchoring)
+        {
+            BeginGroup();
+            AnchorScaling = GUILayout.Toggle(AnchorScaling, "Anchor scaling");
+            EndGroup();
+        }
+
+        MosaixComponent.SphereMasking = GUILayout.Toggle(MosaixComponent.SphereMasking, "Sphere masking");
+        if(MosaixComponent.SphereMasking)
+        {
+            BeginGroup();
+
+            ShowSphere = GUILayout.Toggle(ShowSphere, "Show sphere");
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Sphere fade");
+            MosaixComponent.MaskFade = GUILayout.HorizontalSlider(MosaixComponent.MaskFade, 0, 1);
+            GUILayout.EndHorizontal();
+
+            Vector3 SphereScale = SphereMask.transform.localScale;
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Scale X");
+            SphereScale.x = GUILayout.HorizontalSlider(SphereScale.x, 0.25f, 2);
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Scale Y");
+            SphereScale.y = GUILayout.HorizontalSlider(SphereScale.y, 0.25f, 2);
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Scale Z");
+            SphereScale.z = GUILayout.HorizontalSlider(SphereScale.z, 0.25f, 2);
+            GUILayout.EndHorizontal();
+            SphereMask.transform.localScale = SphereScale;
+
+            EndGroup();
+        }
+
+        MosaixComponent.TextureMasking = GUILayout.Toggle(MosaixComponent.TextureMasking, "Texture masking");
+    }
+
+    int level = 0;
+    void BeginGroup()
+    {
+        ++level;
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(level*15); // left padding
+        GUILayout.BeginVertical();
+    }
+
+    void EndGroup()
+    {
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+        --level;
+    }
+
+    void Refresh()
+    {
+        MosaixComponent.MosaicBlocks = MosaicBlocks;
+        Renderer SphereMaskRenderer = SphereMask.GetComponent<Renderer>();
+        SphereMaskRenderer.enabled = MosaixComponent.SphereMasking && ShowSphere;
+        
+        if(Anchoring)
+            MosaixComponent.AnchorTransform = SphereMask.gameObject;
+        else
+            MosaixComponent.AnchorTransform = null;
+
+        MosaixComponent.ScaleMosaicToAnchorDistance = AnchorScaling;
+        if(Anchoring && AnchorScaling)
+            MosaixComponent.MosaicBlocks *= 5;
+
+        // This shows how you can apply your own effects on top of the mosaic, such as having toon
+        // outlines that aren't mosaiced.
+        MosaixComponent.MosaicMaterial = ShowOutlines? MosaicWithOutlineMaterial:MosaicMaterial;
+    }
+}
---
+++ Assets/Demo/MayaCamera.cs
@@ -0,0 +1,120 @@
+// Rewritten based on https://gist.githubusercontent.com/JISyed/5017805/raw/aa69ce701f3d5a13a9f87880dee776d2208f71cb/MoveCamera.cs
+using UnityEngine;
+
+// TODO Maya zooms exponentially: as you zoom further from the center of interest,
+// zooming moves faster.  We only zoom linearly.
+
+public class MayaCamera: MonoBehaviour 
+{
+    private float TumblingSpeed = 200.0f;
+    private float PanningSpeed = 1.5f;
+    private Vector3 MousePosition;
+    private Vector3 Origin;
+
+    enum MouseMode
+    {
+        Tumbling,
+        Panning,
+        Zooming,
+        None,
+    };
+    MouseMode mode = MouseMode.None;
+
+    static bool GetMouseIntersection(Camera camera, out RaycastHit hit)
+    {
+        Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 world = new Vector3(pos.x, pos.y, camera.nearClipPlane);
+
+        // Find the closest intersection.
+        RaycastHit closest = new RaycastHit();
+        bool found = false;
+        foreach(RaycastHit ray in Physics.RaycastAll(camera.ScreenPointToRay(world)))
+        {
+            if(found && closest.distance < ray.distance)
+                continue;
+            closest = ray;
+            found = true;
+        }
+        hit = closest;
+        return found;
+    }
+
+    void Update() 
+    {
+        Camera camera = Camera.main;
+
+        if(mode == MouseMode.None && Input.GetKey(KeyCode.LeftAlt))
+        {
+            if(Input.GetKey(KeyCode.LeftAlt))
+            {
+                if(Input.GetMouseButton(0))
+                    mode = MouseMode.Tumbling;
+                else if(Input.GetMouseButton(1))
+                    mode = MouseMode.Zooming;
+                else if(Input.GetMouseButton(2))
+                    mode = MouseMode.Panning;
+            }
+
+            if(mode != MouseMode.None)
+            {
+                MousePosition = Input.mousePosition;
+
+                // Find the object the mouse is over.  If there isn't one, keep the old origin.
+                // Maya actually remembers the distance to the most recent click (centerOfInterest)
+                // and uses that as the distance to the origin if you click on a point that doesn't
+                // intersect anything, but we don't currently do that.
+                RaycastHit hit;
+                if(GetMouseIntersection(camera, out hit))
+                    Origin = hit.point;
+            }
+        }
+
+        if(mode == MouseMode.None)
+        {
+            // Handle mouse wheel zooming.
+            float MouseWheelDelta = Input.GetAxis("Mouse ScrollWheel");
+            if(MouseWheelDelta != 0)
+            {
+                bool Forward = MouseWheelDelta > 0;
+                float zoom = .1f * (Forward? +1:-1);
+                ZoomBy(zoom);
+            }
+        }
+        
+        if(mode == MouseMode.Tumbling && !Input.GetMouseButton(0))
+            mode = MouseMode.None;
+        if(mode == MouseMode.Zooming && !Input.GetMouseButton(1))
+            mode = MouseMode.None;
+        if(mode == MouseMode.Panning && !Input.GetMouseButton(2))
+            mode = MouseMode.None;
+
+        Vector3 distance = camera.ScreenToViewportPoint(Input.mousePosition - MousePosition);
+        MousePosition = Input.mousePosition;
+
+        // Rotate camera along X and Y axis
+        if(mode == MouseMode.Tumbling)
+        {
+            Vector3 OriginWorldSpaceRight = transform.localToWorldMatrix.MultiplyVector(Vector3.right);
+            transform.RotateAround(Origin, OriginWorldSpaceRight, -distance.y * TumblingSpeed);
+            transform.RotateAround(Origin, Vector3.up, distance.x * TumblingSpeed);
+        }
+
+        else if(mode == MouseMode.Panning)
+        {
+            Vector3 move = new Vector3(distance.x * -1 * PanningSpeed, distance.y * -1 * PanningSpeed, 0);
+            transform.Translate(move, Space.Self);
+        }
+
+        else if(mode == MouseMode.Zooming)
+        {
+            float zoom = distance.x - distance.y;
+            ZoomBy(zoom);
+        }
+    }
+
+    void ZoomBy(float zoom)
+    {
+        Vector3 move = zoom * transform.forward; 
+        transform.Translate(move, Space.World);
+    }
+}
