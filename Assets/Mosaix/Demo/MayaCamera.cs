75f02d4b527cb21107b50fb25a3d724047888185
Mosaix <unity-effects@users.noreply.github.com>
2017-05-08 22:16:31 -0500

    Increase TumblingSpeed.

--- Assets/Mosaix/Demo/MayaCamera.cs
+++ Assets/Mosaix/Demo/MayaCamera.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class MayaCamera: MonoBehaviour 
 {
-    private float TumblingSpeed = 200.0f;
+    private float TumblingSpeed = 300.0f;
     private float PanningSpeed = 1.5f;
     private Vector3 MousePosition;
     private Vector3 Origin;
