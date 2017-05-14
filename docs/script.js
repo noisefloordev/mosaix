65854705e98a6ef2f5632286277bc47c82e3c4a4
Noise Floor <noisefloordev@users.noreply.github.com>
2017-05-14 03:52:04 -0500

    GA.

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,16 @@
+// GA.  Check the host, so if somebody clones the repository and doesn't change/disable
+// this, we don't send bogus GA requests.
+if(document.location.host == "noisefloordev.github.io")
+{
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-99113878-1', 'auto');
+      ga('send', 'pageview');
+}
+
 (function()
 {
     function closest(e, selector)
